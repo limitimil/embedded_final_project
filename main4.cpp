e11a62c6cb7c7e419a7c1c5b354c44b8d129849f
embedded <embedded@limin-Satellite-L850>
2016-06-15 15:50:47 +0800

    adjust picture orientation, rotate motor with thread

--- main4.cpp
+++ main4.cpp
@@ -4,6 +4,7 @@
 #include "v4l2.h"
 #include "faceDetect.h"
 
+#include <pthread.h>
 #define IMAGEWIDTH 640
 #define IMAGEHEIGHT 480
 using namespace cv;
@@ -13,11 +14,16 @@ Mat makeMat(unsigned char* frame){
 	for(int c=0;c< m.cols;c++)
 	{
 		int location = (r* IMAGEWIDTH +c ) *3;
-		m.at<Vec3b>(r,c) = Vec3b(
+		m.at<Vec3b>(m.rows-r-1,c) = Vec3b(
 			*(frame+location),*(frame+location + 1), *(frame + location +2)
 		);
 	}
 	return m;
+}
+void* thread_routine(void* v){
+	step_degree(*(int*)v);
+	free((int*)v);
+	return 0;
 }
 int main(int argc,const char** argv){
 	//prepare webcam
@@ -30,12 +36,18 @@ int main(int argc,const char** argv){
 	unsigned char* frame_buffer = get_frame_buffer();
 	Mat img;
 	char letter[20];
+
+	pthread_t t;
+	pthread_attr_t attr;
 	while(1){
 		v4l2_photo();
 		img = makeMat(frame_buffer);
 		sendMat(img);
-		int rst = facedetect_main(img);		
-		printf("detect result :%d\n",rst);
+		int* degree = (int*)malloc(sizeof(int));
+		*degree = facedetect_main(img);		
+		printf("detect result :%d\n",*degree);
+		pthread_attr_init(&attr);
+		pthread_create(&t,&attr,thread_routine,(void*) degree);
 		get_letter_20(letter);
 		printf("%s\n", letter);//alert function
 	}
